3d7f2c9eab27083409cc33fde19ef6271417016a
prakharchoudhary <prakharchoudhary>
2017-03-29 14:58:43 +0530

    form cleaned_data functions added

--- app/forms.py
+++ app/forms.py
@@ -21,17 +21,16 @@ class RegistrationForm(forms.Form):
 		widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), 
 		label=_("Password (again)")
 		)
-    
+    	
+	def clean_username(self):
+		try:
+			user = User.objects.get(username__iexact=self.cleaned_data['username'])
+		except User.DoesNotExist:
+			return self.cleaned_data['username']
+		raise forms.ValidationError(_("The username already exists. Please try another one."))
 
-def clean_username(self):
-	try:
-		user = User.objects.get(username__iexact=self.cleaned_data['username'])
-	except User.DoesNotExist:
-		return self.cleaned_data['username']
-	raise forms.ValidationError(_("The username already exists. Please try another one."))
-
-def clean(self):
-	if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
-		if self.cleaned_data['password1'] != self.cleaned_data['password2']:
-			raise forms.ValidationError(_("The two password fields did not match."))
-	return self.cleaned_data
+	def clean(self):
+		if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
+			if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+				raise forms.ValidationError(_("The two password fields did not match."))
+		return self.cleaned_data
